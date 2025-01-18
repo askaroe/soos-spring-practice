b982e697c3d5f09b7e9812d5097425ecb76afe57
Askar <askar4ik123@gmail.com>
2025-01-19 02:07:44 +0600

    updated script

--- shell/create-file.sh
+++ shell/create-file.sh
@@ -1,5 +1,7 @@
 HOME=/home/admin/soos/soos-spring-practice
 
+mkdir -p $HOME/test/target
+
 touch $HOME/test/target/my.txt
 
 cat $HOME/numbers.txt | python3 $HOME/python/creator.py >> $HOME/test/target/my.txt
